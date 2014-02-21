a7f0771c2b8d0f3c4fe1fc5ee3feff016cd492b5
gr4v3 <gr4v3m4n@gmail.com>
2014-02-21 20:51:10 +0000

    fixed the path for the socket library.

--- controllers/index.php
+++ controllers/index.php
@@ -34,12 +34,12 @@ class Index extends Load {
     public function start() {
         //$this->include->folder('sockets');
 
-        $this->include->file('sockets/socket');
-        $this->include->file('sockets/socketClient');
-        $this->include->file('sockets/socketDaemon');
-        $this->include->file('sockets/socketServer');
-        $this->include->file('sockets/socketServerClient');
-        $this->include->file('configServer');
+        $this->include->file('includes/sockets/socket');
+        $this->include->file('includes/sockets/socketClient');
+        $this->include->file('includes/sockets/socketDaemon');
+        $this->include->file('includes/sockets/socketServer');
+        $this->include->file('includes/sockets/socketServerClient');
+        $this->include->file('includes/configServer');
         
         ini_set('mbstring.func_overload', '0');
         ini_set('output_handler', '');
